f99834f684258f76c2c01e1ea83fa82e14bfd503
Aleksandar Simic <asimic@gmail.com>
2013-12-15 13:34:08 +0000

    Attempt at making it more succinct
    It feels like duplication having both get-fields &
    get-fields-and-values, as you can get just the keys by using:
    
    (keys (get-fields "file.pdf"))

--- src/pdfboxing/form.clj
+++ src/pdfboxing/form.clj
@@ -5,15 +5,6 @@
     [org.apache.pdfbox.pdmodel.interactive.form PDAcroForm PDAppearance PDField PDSignatureField]))
 
 (defn get-fields
-  "get all the field names of a PDF document"
-  [pdfdoc]
-  (with-open [doc (PDDocument/load pdfdoc)]
-    (let [catalog (.getDocumentCatalog doc)
-          form (.getAcroForm catalog)
-          fields (.getFields form)]
-    (vec (map #(.getPartialName %) fields)))))
-
-(defn get-fields-and-values
   "get all the field names and their values from a PDF document"
   [pdfdoc]
   (with-open [doc (PDDocument/load pdfdoc)]
