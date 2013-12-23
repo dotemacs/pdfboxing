1a9886b55a14b7f7a96700eac9f4c82d89dbb2c5
Aleksandar Simic <asimic@gmail.com>
2013-12-23 21:56:37 +0000

    Added about-doc function

--- src/pdfboxing/info.clj
+++ src/pdfboxing/info.clj
@@ -1,5 +1,7 @@
 (ns pdfboxing.info
+  (:require [clojure.string :as string])
   (:import
+   [clojure.lang Reflector]
    [org.apache.pdfbox.pdmodel PDDocument PDDocumentCatalog PDDocumentInformation]))
 
 (defn page-number
@@ -7,3 +9,22 @@
   [pdfdoc]
   (with-open [doc (PDDocument/load pdfdoc)]
     (.getNumberOfPages doc)))
+
+
+(defn- getter
+  "private helper function to be used by about-doc"
+  [obj method-str & args]
+  (let [get-method (str "get" (string/capitalize method-str))]
+    (Reflector/invokeInstanceMethod obj get-method (to-array args))))
+
+(def info-fields ["title" "author" "subject" "keywords" "creator" "producer"])
+
+(defn about-doc
+  "get the basic information about the given document
+   the values provided are for the fields defined by
+   info-fields"
+  [pdfdoc]
+  (with-open [doc (PDDocument/load pdfdoc)]
+    (let [info (.getDocumentInformation doc)]
+      (into {}
+            (map #(hash-map (str %1) (getter info %1)) info-fields)))))
--- test/pdfboxing/info_test.clj
+++ test/pdfboxing/info_test.clj
@@ -1,7 +1,20 @@
 (ns pdfboxing.info-test
   (:require [clojure.test :refer :all]
-            [clojure.java.io :as io]
             [pdfboxing.info :refer :all]))
 
 (deftest document-info
-  (is (= 1 (page-number "test/pdfs/interactiveform.pdf"))))
+  (testing "page count"
+    (is (= 1 (page-number "test/pdfs/interactiveform.pdf"))))
+  (testing "field count"
+    (is (= 6 (count (about-doc "test/pdfs/interactiveform.pdf")))))
+  (testing "for the key names"
+    (is (= ["title" "author" "subject" "keywords" "creator" "producer"]
+           (keys (about-doc "test/pdfs/interactiveform.pdf")))))
+  (testing "if the full document info matches"
+    (is (= {"title" "Example of an Interactive PDF Form",
+            "author" nil,
+            "subject" "PDF forms",
+            "keywords" "forms, flat, fillable",
+            "creator" "Adobe InDesign CS3 (5.0.4)",
+            "producer" "Mac OS X 10.8.5 Quartz PDFContext"}
+           (about-doc "test/pdfs/interactiveform.pdf")))))
