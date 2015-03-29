c187175ff01364c09ed6226c24c48ecf5b7fba7c
Aleksandar Simic <asimic@gmail.com>
2015-03-29 20:36:44 +0100

    Upgraded Clojure to 1.6.0
    There's no need to lag behind and 1.6.0 has been out for over a year.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/bsd-license.php"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.pdfbox/pdfbox "1.8.3"]
-                 [org.apache.pdfbox/preflight "1.8.3"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.apache.pdfbox/pdfbox "1.8.9"]
+                 [org.apache.pdfbox/preflight "1.8.9"]])
