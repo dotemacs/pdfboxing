1e84ca191235044dfdee30d8a14c229cbaed487e
Aleksandar Simic <asimic@gmail.com>
2015-04-01 19:57:28 +0100

    Added drawing capbility
    This change enables drawing of lines on a PDF document at a given page
    number.
    
    Currently only black line is drawn, but the ability to specify any
    colour using RGB scheme should be allowed. Along with specifying the
    line thickness of the line to be drawn. The functions that handle that
    are already implemented it's just deciding how to pass those arguments
    to draw-line function so that it doesn't become too verbose. As in, too
    many parameters that it has to take.

---
+++ src/pdfboxing/draw.clj
@@ -0,0 +1,54 @@
+(ns pdfboxing.draw
+  (:import [org.apache.pdfbox.pdmodel PDDocument PDDocumentCatalog PDPage]
+           [org.apache.pdfbox.pdmodel.edit PDPageContentStream]))
+
+(defn get-catalog
+  "get a catalog from a PDF document"
+  [document]
+  (.getDocumentCatalog document))
+
+(defn get-page
+  "get a particular page from a catalog"
+  [catalog page-number]
+  (.get (.getAllPages catalog) page-number))
+
+(defn get-content-stream
+  "take the read in PDF document and a page number and return content
+  stream which will be manipulated"
+  [document page-number]
+  (let [catalog (get-catalog document)
+        page (get-page catalog page-number)
+        content-stream (PDPageContentStream. document page true true)]
+    content-stream))
+
+(defn use-rgb-colour
+  "take content-stream and use RGB colour to draw on it
+   e.g. 0 0 0 for black"
+  [content-stream r g b]
+  (.setStrokingColor content-stream r g b))
+
+(defn set-line-width
+  "the size of the line to be drawn
+   e.g. 3"
+  [content-stream line-width]
+  (.setLineWidth content-stream line-width))
+
+(defn draw-line-at-coordinates
+  "Take content-stream and coordinates for line to be drawn.
+   Coordinates are a hash map in the format of: x, y, x1, y1."
+  [content-stream coordinates]
+  (.drawLine content-stream
+             (:x coordinates)
+             (:y coordinates)
+             (:x1 coordinates)
+             (:y1 coordinates)))
+
+(defn draw-line
+  "draw a line on input-pdf's page, producing a output-pdf document"
+  [& {:keys [input-pdf page-number output-pdf coordinates]}]
+  (with-open [document (PDDocument/load input-pdf)]
+    (with-open [content-stream (get-content-stream document page-number)]
+      (use-rgb-colour content-stream 0 0 0)
+      (set-line-width content-stream 3)
+      (draw-line-at-coordinates content-stream coordinates))
+      (.save document output-pdf)))
