3af831845411ef2e07cdcfb8e9afb12c3c02129e
Александар Симић <asimic@gmail.com>
2015-04-05 12:22:35 +0100

    Checking if documents passed in are PDFs
    Also using the is-pdf? function in the test.

--- src/pdfboxing/merge.clj
+++ src/pdfboxing/merge.clj
@@ -1,15 +1,30 @@
 (ns pdfboxing.merge
+  (:require [pdfboxing.common :as common])
   (:import [org.apache.pdfbox.util PDFMergerUtility]
            [java.io File FileInputStream]))
 
+(defn throw-exception
+  [message]
+  (throw (IllegalArgumentException. message)))
+
+(defn check-if-present
+  "Check if the input & output file names where supplied"
+  [input output]
+  (if (some true? (map empty? [input output]))
+    (throw-exception "argument can't be empty")))
+
+(defn check-for-pdfs
+  "Check if all the files supplied are actual PDFs."
+  [files]
+  (if (some false? (map common/is-pdf? files))
+    (throw-exception "the files supplied need to be PDFs")
+    true))
+
 (defn arg-check [output input]
-  (if (empty? output)
-    (throw (IllegalArgumentException. "output - required argument"))
-    (if (empty? input)
-      (throw (IllegalArgumentException. "input - required argument"))
-      (if (sequential? input)
-        true
-        (throw (IllegalArgumentException. "input - needs to be sequential"))))))
+  (check-if-present input output)
+  (if (sequential? input)
+    (check-for-pdfs input)
+    (throw-exception "input - needs to be sequential")))
 
 (defn merge-pdfs
   "merge multiple PDFs into output file"
--- test/pdfboxing/merge_test.clj
+++ test/pdfboxing/merge_test.clj
@@ -1,13 +1,14 @@
 (ns pdfboxing.merge-test
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
-            [pdfboxing.merge :refer :all]))
+            [pdfboxing.merge :refer :all]
+            [pdfboxing.common :as common]))
 
 (deftest input-output-argument-check
   (is (thrown? IllegalArgumentException (arg-check)))
   (is (thrown? IllegalArgumentException (arg-check "input")))
   (is (thrown? IllegalArgumentException (arg-check "input" "output")))
-  (is (true? (arg-check "output" ["a.pdf" "b.pdf"]))))
+  (is (true? (arg-check "output" ["test/pdfs/clojure-1.pdf" "test/pdfs/clojure-2.pdf"]))))
 
 
 (deftest pdf-merge-exit-status
@@ -19,7 +20,8 @@
         merging-outcome (merge-pdfs :output file
                                     :input ["test/pdfs/clojure-1.pdf" "test/pdfs/clojure-2.pdf"])
         merged-pdf-file (.exists (io/as-file file))]
-    (is (true? merged-pdf-file))))
+    (is (true? merged-pdf-file))
+    (is (true? (common/is-pdf? file)))))
 
 ;; clean up
 (defn clean-up [file]
