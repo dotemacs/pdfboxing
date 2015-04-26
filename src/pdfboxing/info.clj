2c89655054a804ae4bbfc1c3da0137adf0533150
Kazuo Koga <obiwanko@me.com>
2015-04-26 21:00:36 +0900

    Can specify keys which you are interested in

--- src/pdfboxing/info.clj
+++ src/pdfboxing/info.clj
@@ -14,17 +14,23 @@
 (defn- getter
   "private helper function to be used by about-doc"
   [obj method-str]
-  (let [get-method (str "get" (string/join (map string/capitalize (string/split method-str #"-"))))]
+  (let [get-method (str "get"
+                        (string/join
+                         (map string/capitalize
+                              (string/split method-str #"-"))))]
     (Reflector/invokeInstanceMethod obj get-method (to-array []))))
 
 (defn about-doc
   "get the basic information about the given document
    the values provided are for the fields defined by
    info-fields"
-  [pdfdoc]
+  [pdfdoc & {:keys [keys]
+             :or {keys ["title" "author" "subject" "keywords"
+                        "creator" "producer" "trapped" "metadata-keys"
+                        "creation-date" "modification-date"]}}]
   (with-open [doc (common/load-pdf pdfdoc)]
     (let [info (.getDocumentInformation doc)
-          info-fields ["title" "author" "subject" "keywords" "creator" "producer" "trapped" "metadata-keys" "creation-date" "modification-date"]]
+          info-fields keys]
       (into {}
             (map #(hash-map (str %1) (getter info %1)) info-fields)))))
 
--- test/pdfboxing/info_test.clj
+++ test/pdfboxing/info_test.clj
@@ -8,7 +8,9 @@
   (testing "field count"
     (is (= 10 (count (about-doc "test/pdfs/interactiveform.pdf")))))
   (testing "for the key names"
-    (is (= #{"title" "author" "subject" "keywords" "creator" "producer" "trapped" "metadata-keys" "creation-date" "modification-date"}
+    (is (= #{"title" "author" "subject" "keywords" "creator" "producer"
+             "trapped" "metadata-keys" "creation-date"
+             "modification-date"}
            (set (keys (about-doc "test/pdfs/interactiveform.pdf"))))))
   (testing "if the full document info matches"
     (is (= {"title" "Example of an Interactive PDF Form",
@@ -16,17 +18,25 @@
             "subject" "PDF forms",
             "keywords" "forms, flat, fillable",
             "trapped" nil,
-            "metadata-keys" #{"AAPL:Keywords" "CreationDate" "Creator" "Keywords" "ModDate" "Producer" "Subject" "Title"}
+            "metadata-keys" #{"AAPL:Keywords" "CreationDate"
+                              "Creator" "Keywords" "ModDate" "Producer"
+                              "Subject" "Title"}
             "creator" "Adobe InDesign CS3 (5.0.4)",
             "producer" "Mac OS X 10.8.5 Quartz PDFContext"}
            (dissoc (about-doc "test/pdfs/interactiveform.pdf")
                    "creation-date" ;#inst "2013-12-14T13:44:39.000+00:00"
                    "modification-date")))) ;#inst "2013-12-14T13:44:39.000+00:00"
+  (testing "only requested keys value matches"
+    (is (= {"title" "Example of an Interactive PDF Form",
+            "subject" "PDF forms"}
+           (about-doc "test/pdfs/interactiveform.pdf"
+                      :keys ["title" "subject"]))))
   (testing "for the metadata value"
     (is (= "Example of an Interactive PDF Form"
            (metadata-value "test/pdfs/interactiveform.pdf" "Title")))
     (is (= "D:20131214134439Z00'00'"
-           (metadata-value "test/pdfs/interactiveform.pdf" "CreationDate")))
+           (metadata-value "test/pdfs/interactiveform.pdf"
+                           "CreationDate")))
     (is (= {"AAPL:Keywords" nil,
             "CreationDate" "D:20131214134439Z00'00'",
             "Creator" "Adobe InDesign CS3 (5.0.4)",
