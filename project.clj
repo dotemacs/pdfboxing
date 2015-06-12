aa901739d18bad6611e06c4f3bd2bb7697d6a046
Paul Nakata <pmn@github.com>
2015-06-12 12:02:42 -0700

    Bump version to 0.1.6 to reflect merge of PDF split functionality.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pdfboxing "0.1.5"
+(defproject pdfboxing "0.1.6"
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
