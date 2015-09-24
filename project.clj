40617e55e6ab9ff700ef0b33e3e61f4191fb2be7
Александар Симић <asimic@gmail.com>
2015-09-24 12:49:12 +0100

    Updated to pdfbox 1.8.10
    Want to stay current with the latest stable dependencies

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/bsd-license.php"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.apache.pdfbox/pdfbox "1.8.9"]
+                 [org.apache.pdfbox/pdfbox "1.8.10"]
                  [org.apache.pdfbox/preflight "1.8.9"]])
