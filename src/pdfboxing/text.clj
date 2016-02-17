b6b981916f59e9f5b0583b7a471f7bb718274513
mil <miles.sandlar@gmail.com>
2016-02-17 03:29:46 -0600

    Moves .getText call in extract to separate defn This allows for new extract-from-pddocument defn to be used directly without reading a PDF at the same time. This is helpful when you need to perform text extraction on a page-by-page basis after a split-pdf for example.

--- src/pdfboxing/text.clj
+++ src/pdfboxing/text.clj
@@ -2,9 +2,15 @@
   (:require [pdfboxing.common :as common])
   (:import [org.apache.pdfbox.util PDFTextStripper]))
 
+(defn extract-from-pddocument
+  "get text from a PDDocument"
+  [pddoc]
+    (if (instance? org.apache.pdfbox.pdmodel.PDDocument pddoc)
+      (let [stripper (new PDFTextStripper)]
+        (.getText stripper pddoc))))
+
 (defn extract
-  "get text from a PDF document"
+  "get text from a PDF document filesystem path"
   [pdfdoc]
   (with-open [doc (common/load-pdf pdfdoc)]
-    (let [stripper (new PDFTextStripper)]
-      (.getText stripper doc))))
+    (extract-from-pddocument doc)))
