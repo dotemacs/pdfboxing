79ee00c48ec1a0c7450ccaae1e82028b04099a0a
Александар Симић <asimic@gmail.com>
2016-03-05 00:26:49 +0000

    Checks for the second line of a PDF file
    The second line should be 5 or more characters in lenght. Started to add
    checks for it.

--- src/pdfboxing/util.clj
+++ src/pdfboxing/util.clj
@@ -4,3 +4,8 @@
   "validate the first line of a given PDF file"
     [first-line]
     (.matches first-line "%PDF-1\\.[1-9]"))
+
+(defn line-long-enough?
+  "check the length of the second line of a given PDF file"
+    [second-line]
+    (>= 5 (.length second-line)))
--- test/pdfboxing/util_test.clj
+++ test/pdfboxing/util_test.clj
@@ -6,3 +6,8 @@
   (testing "the contents of the first line"
     (is (false? (first-line-valid? "%PDF-2")))
     (is (true? (first-line-valid? "%PDF-1.9")))))
+
+(deftest lenght-of-line
+  (testing "line length of the second line of a given PDF file"
+    (is (false? (line-long-enough? "123456789")))
+    (is (true? (line-long-enough? "12345")))))
