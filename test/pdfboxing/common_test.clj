d5ead535cb36282ec561ebc42ed8d6267c27a307
mil <miles.sandlar@gmail.com>
2016-03-19 15:08:11 -0500

    adds tests for PDFDocument protocol obtain-document function c

--- test/pdfboxing/common_test.clj
+++ test/pdfboxing/common_test.clj
@@ -6,3 +6,15 @@
   (testing "If document supplied is a PDF"
     (is (false? (is-pdf? "test/pdfs/a.txt")))
     (is (true? (is-pdf? "test/pdfs/hello.pdf")))))
+
+(deftest obtain-document-returns-pddocument-if-provided-string-check
+  (testing "obtain-document returns a PDDocument if a string path to a PDF is a supplied"
+    (is (true?  (instance?
+                   org.apache.pdfbox.pdmodel.PDDocument
+                   (obtain-document "test/pdfs/hello.pdf"))))))
+
+(deftest obtain-document-returns-pddocument-if-provided-pddocument-check
+  (testing "obtain-document returns a PDDocument if PDDocument is provided"
+    (is (true?  (instance?
+                   org.apache.pdfbox.pdmodel.PDDocument
+                   (obtain-document (obtain-document "test/pdfs/hello.pdf")))))))
