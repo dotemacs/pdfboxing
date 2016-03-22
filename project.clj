7f8152370f580063326bf4012c2756729933fae7
Александар Симић <asimic@gmail.com>
2016-03-22 07:45:51 +0000

    Release 0.1.9
    After upgrading to PDFBox 2.0.0 and Clojure 1.8.0, bumping up the
    release version to 0.1.9.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pdfboxing "0.1.8"
+(defproject pdfboxing "0.1.9"
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
