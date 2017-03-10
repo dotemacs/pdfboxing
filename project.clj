883646b7552197a9ea6144786d099f03f5f4c828
Александар Симић <asimic@gmail.com>
2017-03-10 06:46:55 +0000

    Touched up the licence URL
    Minor, but removed the URL suffix as it looks better

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
-            :url "http://www.opensource.org/licenses/bsd-license.php"}
+            :url "http://www.opensource.org/licenses/bsd-license"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.pdfbox/pdfbox "2.0.4"]
                  [org.apache.pdfbox/preflight "2.0.4"]])
