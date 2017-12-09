35469a55126a490f42c91baf9f0052c7fc2b885c
Александар Симић <asimic@gmail.com>
2017-12-09 20:41:40 +0000

    Upgraded to Clojure 1.9.0
    Upgraded to the latest stable Cloljure release

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/bsd-license"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.pdfbox/pdfbox "2.0.8"]
                  [org.apache.pdfbox/preflight "2.0.8"]])
