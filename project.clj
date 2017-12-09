ef4eb302f5b659b4c6749b7f438ff80aef926f92
Александар Симић <asimic@gmail.com>
2017-12-09 20:20:27 +0000

    Updated dependencies
    Doing it one release up from 2.0.6 to 2.0.7

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject pdfboxing "0.1.13"
+(defproject pdfboxing "0.1.14-SNAPSHOT"
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/bsd-license"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.pdfbox/pdfbox "2.0.6"]
-                 [org.apache.pdfbox/preflight "2.0.6"]])
+                 [org.apache.pdfbox/pdfbox "2.0.7"]
+                 [org.apache.pdfbox/preflight "2.0.7"]])
