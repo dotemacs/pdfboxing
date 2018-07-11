b7a0540f912b39c5e07cbf6d75475e63534a7055
Petr Mensik <petr.mensik1@gmail.com>
2018-07-12 00:40:07 +0200

    Add image merging to PDF

--- test/pdfboxing/merge_test.clj
+++ test/pdfboxing/merge_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [clojure.test :refer [deftest is]]
             [pdfboxing.common :as common]
-            [pdfboxing.merge :refer [arg-check merge-pdfs]]))
+            [pdfboxing.info :refer [page-number]]
+            [pdfboxing.merge :refer [arg-check merge-pdfs merge-images-from-path]]))
 
 (deftest input-output-argument-check
   (is (thrown? IllegalArgumentException (arg-check)))
@@ -10,10 +11,9 @@
   (is (thrown? IllegalArgumentException (arg-check "input" "output")))
   (is (true? (arg-check "output" ["test/pdfs/clojure-1.pdf" "test/pdfs/clojure-2.pdf"]))))
 
-
 (deftest pdf-merge-exit-status
-        (is (nil? (merge-pdfs :output "test/pdfs/foo.pdf"
-                              :input ["test/pdfs/clojure-1.pdf" "test/pdfs/clojure-2.pdf"]))))
+  (is (nil? (merge-pdfs :output "test/pdfs/foo.pdf"
+                        :input ["test/pdfs/clojure-1.pdf" "test/pdfs/clojure-2.pdf"]))))
 
 (deftest pdf-file-merge
   (let [file "test/pdfs/foo.pdf"
@@ -23,10 +23,17 @@
     (is (true? merged-pdf-file))
     (is (true? (common/is-pdf? file)))))
 
+(deftest pdf-image-merge
+  (let [file "test/pdfs/image_merge.pdf"
+        images ["test/images/image1.jpg" "test/images/image2.jpg"]]
+    (merge-images-from-path images file)
+    (is (= (page-number file) 2))))
+
 ;; clean up
 (defn clean-up [file]
   (if (.exists (io/as-file file))
     (io/delete-file file)))
 
 (deftest cleaner
-  (clean-up "test/pdfs/foo.pdf"))
+  (clean-up "test/pdfs/foo.pdf")
+  (clean-up "test/pdfs/image_merge.pdf"))
