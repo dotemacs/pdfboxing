845d053a678b5972c2011d5fb3b791a4fe84edc9
Александар Симић <asimic@gmail.com>
2019-07-09 22:37:35 +0100

    Release 0.1.14
    Making a new release version 0.1.14, which brings the following
    changes:
    
    - bug fix so that splitting of PDFs into PDDocuments works again
    - upgrade of pdfbox to 2.0.16
    - better reporting of errors for missing fields for PDF forms
    - removed dependency on preflight library
    - updated the testing setup on TravisCI

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pdfboxing "0.1.14.1-SNAPSHOT"
+(defproject pdfboxing "0.1.14"
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
