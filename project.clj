8dc4feee9964c17169300f30b53807d850258cda
Александар Симић <asimic@gmail.com>
2020-02-23 12:28:27 +0000

    Cutting a new version 0.1.15-SNAPSHOT
    So that the new changes can be tried out

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pdfboxing "0.1.14"
+(defproject pdfboxing "0.1.15-SNAPSHOT"
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
