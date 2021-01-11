5e2a817134aec2100c259734b178656c11cbd1bd
Pavlos Melissinos <pmelissi@gmail.com>
2021-01-12 01:40:08 +0200

    Extract pdf text by areas

--- src/pdfboxing/text.clj
+++ src/pdfboxing/text.clj
@@ -1,6 +1,8 @@
 (ns pdfboxing.text
   (:require [pdfboxing.common :as common])
-  (:import org.apache.pdfbox.text.PDFTextStripper))
+  (:import (org.apache.pdfbox.text PDFTextStripper
+                                   PDFTextStripperByArea)
+           (java.awt Rectangle)))
 
 (defn extract
   "get text from a PDF document"
@@ -8,3 +10,18 @@
   (with-open [doc (common/obtain-document pdfdoc)]
     (-> (PDFTextStripper.)
         (.getText doc))))
+
+(defn- area-text [doc {:keys [x y w h page-number] :as area}]
+  (let [page-number  (or page-number 0)
+        rectangle    (Rectangle. x y w h)
+        pdpage       (.getPage doc page-number)
+        textstripper (doto (PDFTextStripperByArea.)
+                       (.addRegion "region" rectangle)
+                       (.extractRegions pdpage))]
+    (.getTextForRegion textstripper "region")))
+
+(defn extract-by-areas
+  "get text from a specified area of a PDF document"
+  [pdfdoc areas]
+  (with-open [doc (common/obtain-document pdfdoc)]
+    (doall (map #(area-text doc %) areas))))
--- test/pdfboxing/text_test.clj
+++ test/pdfboxing/text_test.clj
@@ -1,9 +1,24 @@
 (ns pdfboxing.text-test
   (:require [clojure.test :refer [deftest is]]
-            [pdfboxing.text :refer [extract]]))
+            [pdfboxing.text :refer [extract extract-by-areas]]))
 
 (def line-separator (System/getProperty "line.separator"))
 
 (deftest text-extraction
   (is (= (str "Hello, this is pdfboxing.text" line-separator)
          (extract "test/pdfs/hello.pdf"))))
+
+(deftest text-extract-by-areas
+  (let [areas [{:x           150
+                :y           100
+                :w           260
+                :h           40
+                :page-number 0}
+               {:x           380
+                :y           500
+                :w           27
+                :h           23
+                :page-number 4}]]
+    (is (= ["Clojure 1.6 Cheat Sheet (v21)\n"
+            "*ns*\n"]
+           (extract-by-areas "test/pdfs/multi-page.pdf" areas)))))
