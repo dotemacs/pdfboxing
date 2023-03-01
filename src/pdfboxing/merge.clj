4c8311bf7250e6ceb8f4cfd588603c09d94822f4
Oliver Marshall <olivershawmarshall@gmail.com>
2023-03-01 13:34:45 +0000

    feat(merge): allow merging from streams to streams

--- src/pdfboxing/merge.clj
+++ src/pdfboxing/merge.clj
@@ -1,6 +1,6 @@
 (ns pdfboxing.merge
   (:require [pdfboxing.common :as common])
-  (:import (java.io File FileInputStream)
+  (:import (java.io InputStream OutputStream)
            (org.apache.pdfbox.multipdf PDFMergerUtility)
            (org.apache.pdfbox.pdmodel PDDocument
                                       PDPage
@@ -15,13 +15,19 @@
 (defn check-if-present
   "Check if the input & output file names where supplied"
   [input output]
-  (when (some true? (map empty? [input output]))
+  (when (or (nil? input) (nil? output))
     (throw-exception "argument can't be empty")))
+
+(defn input-stream-or-pdf?
+  "Checks if the file supplied is a PDF or an InputStream"
+  [file]
+  (or (instance? InputStream file)
+      (common/is-pdf? file)))
 
 (defn check-for-pdfs
   "Check if all the files supplied are actual PDFs."
   [files]
-  (if (some false? (map common/is-pdf? files))
+  (if (some false? (map input-stream-or-pdf? files))
     (throw-exception "the files supplied need to be PDFs")
     true))
 
@@ -36,9 +42,14 @@
   [& {:keys [output input]}]
   {:pre [(arg-check output input)]}
   (let [merger (PDFMergerUtility.)]
-    (doseq [f input]
-      (.addSource merger (FileInputStream. (File. f))))
-    (.setDestinationFileName merger output)
+    (doseq [source input]
+      (.addSource merger source))
+    (cond
+      (instance? OutputStream output)
+      (.setDestinationStream merger output)
+
+      :else
+      (.setDestinationFileName merger output))
     (.mergeDocuments merger)))
 
 (defn- add-image-to-page
