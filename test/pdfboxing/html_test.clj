cc5e0fa4e07bfc27f12b8dc8322df99e4fa5e192
Александар Симић <a@repl.ist>
2023-03-10 17:15:35 +0000

    Another issue caught by clj-kondo
    Test fix for pdfboxing.html, the test wasn't really checking for
    equality

--- test/pdfboxing/html_test.clj
+++ test/pdfboxing/html_test.clj
@@ -2,17 +2,16 @@
   (:require [clojure.test :refer [deftest is]]
             [pdfboxing.html :refer [pdf-to-html]]))
 
-(def line-separator (System/getProperty "line.separator"))
-
 (deftest pdf-to-html-tool
   (let [_html-doc (pdf-to-html "test/pdfs/hello.pdf")]
-    (is (= (str "
-<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"
+    (is (= (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"
 \"http://www.w3.org/TR/html4/loose.dtd\">
 <html><head><title></title>
 <meta http-equiv=\"Content-Type\" content=\"text/html; charset=\"UTF-8\">
-                </head>
-                <body>
-                <div style=\"page-break-before:always; page-break-after:always\"><div><p>Hello, this is pdfboxing.text</p>
-                </div></div>
-                </body></html>" line-separator)) (slurp "test/pdfs/hello.html"))))
+</head>
+<body>
+<div style=\"page-break-before:always; page-break-after:always\"><div><p>Hello, this is pdfboxing.text</p>
+
+</div></div>
+</body></html>")
+           (slurp "test/pdfs/hello.html")))))
