2b9f4c7549d78b0aba4fcef638b6a04cc60469cf
Александар Симић <a@repl.ist>
2023-03-10 17:39:09 +0000

    Further linting issues fixed
    All were caught by linting test directory via clj-kondo

--- test/pdfboxing/split_test.clj
+++ test/pdfboxing/split_test.clj
@@ -3,26 +3,25 @@
             [pdfboxing.split :as split]))
 
 (deftest pdf-input-arg-check
-  "Test to ensure that the invalid input throws meaningful exception"
   (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :input 12 :start 1 :end 2)))
   (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :input 12)))
   (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :input 12 :start 1)))
   (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :input 12 :end 2)))
   (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :input 12 :start 0)))
   (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :start 1 :end 2)))
-  (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :input []))))
+  (is (thrown? IllegalArgumentException #"input must be a string" (split/split-pdf :input [])))
+  "Test to ensure that the invalid input throws meaningful exception")
 
 (deftest pdf-start-arg-check
-  "Test to ensure that :start and :end are valid integers"
   (is (thrown? IllegalArgumentException #":start and :end may only be integers" (split/split-pdf :input "test/pdfs/multi-page.pdf" :start :end)))
   (is (thrown? IllegalArgumentException #":start and :end may only be integers" (split/split-pdf :input "test/pdfs/multi-page.pdf" :end "foo")))
   (is (thrown? IllegalArgumentException #":start and :end may only be integers" (split/split-pdf :input "test/pdfs/multi-page.pdf" :start "bar")))
-  (is (thrown? IllegalArgumentException #":start and :end may only be integers" (split/split-pdf :input "test/pdfs/multi-page.pdf" :end "foo" :start "bar"))))
+  (is (thrown? IllegalArgumentException #":start and :end may only be integers" (split/split-pdf :input "test/pdfs/multi-page.pdf" :end "foo" :start "bar")))
+  "Test to ensure that :start and :end are valid integers")
 
 
 (deftest pdf-file-split
-  "Test that the multi-page PDF was turned into multiple PDDocuments"
-  (let [file "tests/pdfs/multi-page.pdf"
+  (let [_file "tests/pdfs/multi-page.pdf"
         splitting-outcome (split/split-pdf :input "test/pdfs/multi-page.pdf")]
     (is (true? (> (count splitting-outcome) 0)))
     (doall (for [doc splitting-outcome]
@@ -30,4 +29,5 @@
                (is (instance? org.apache.pdfbox.pdmodel.PDDocument doc))
                (finally
                  (when (instance? org.apache.pdfbox.pdmodel.PDDocument doc)
-                   (.close doc))))))))
+                   (.close doc)))))))
+  "Test that the multi-page PDF was turned into multiple PDDocuments")
