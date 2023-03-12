700cdce001dbe3f44d8ea548d94afdb6798eecb6
Александар Симић <a@repl.ist>
2023-03-12 07:38:46 +0000

    Updated the link to the license
    The old one was 404-ing

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure PDF manipulation library & wrapper for PDFBox"
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
-            :url "http://www.opensource.org/licenses/bsd-license"}
+            :url "https://opensource.org/license/bsd-3-clause/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.apache.pdfbox/pdfbox "2.0.27"]
                  [org.apache.pdfbox/pdfbox-tools "2.0.27"]])
