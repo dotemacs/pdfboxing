059753d0d9652597199aad43a4cb02e3fc26ae51
Ray McDermott <ray.mcdermott@fundingcircle.com>
2024-04-15 18:09:24 +0200

    bump pdfbox to 3.0.1 migrate code, tests pass

--- src/pdfboxing/common.clj
+++ src/pdfboxing/common.clj
@@ -1,26 +1,35 @@
 (ns pdfboxing.common
   (:require [clojure.java.io :as io])
   (:import (java.io File)
-           (org.apache.pdfbox.pdmodel PDDocument)
-           (org.apache.pdfbox.io RandomAccessFile)
-           (org.apache.pdfbox.pdfparser PDFParser)))
+           (org.apache.pdfbox Loader)
+           (org.apache.pdfbox.io RandomAccessReadBufferedFile)))
+
+
+(defn load-pdf-from-media [pdf-file-or-path]
+  (try
+    (-> pdf-file-or-path
+        ^File (io/as-file)
+        (Loader/loadPDF))
+    (catch Exception _)))
+
+(defn load-pdf-from-bytes [^bytes pdf-bytes]
+  (Loader/loadPDF pdf-bytes))
 
 (defn try-get-as-pdf
   "Try and get the pdf-file-or-path as a PDF.
   Returns nil if pdf-file-or-path could not be loaded as a PDF."
   [pdf-file-or-path]
-  (let [^File pdf-file (io/as-file pdf-file-or-path)
-        random-access-file (RandomAccessFile. pdf-file "r")
-        parser (PDFParser. random-access-file)]
-    (try
-      (.parse parser)
-      (.getPDDocument parser)
-      (catch Exception _))))
+  (try
+    (-> pdf-file-or-path
+        ^File (io/as-file)
+        (RandomAccessReadBufferedFile.)
+        (Loader/loadPDF))
+    (catch Exception _)))
 
 (defn is-pdf?
   "Confirm that the PDF supplied is really a PDF"
   [pdf-file-or-path]
-  (if-let [pdf (try-get-as-pdf pdf-file-or-path)]
+  (if-let [pdf (load-pdf-from-media pdf-file-or-path)]
     (try
       (not (nil? pdf))
       (finally
@@ -29,31 +38,27 @@
 
 (defn load-pdf
   "Load a given PDF only after checking if it really is a PDF"
-  [pdf-file-or-path]
-  (if-let [pdf (try-get-as-pdf pdf-file-or-path)]
+  [bytes-pdf-file-or-path]
+  (if-let [pdf (try-get-as-pdf bytes-pdf-file-or-path)]
     pdf
-    (throw (IllegalArgumentException. (format "%s is not a PDF file" pdf-file-or-path)))))
+    (throw (IllegalArgumentException. (format "%s is not a PDF file" bytes-pdf-file-or-path)))))
 
 (defprotocol PDFDocument
   "return an object from which text can be extracted"
   (obtain-document [source]))
 
 (extend-protocol PDFDocument
-  (Class/forName "[B") ;; byte-array
+  (Class/forName "[B")                                      ;; byte-array
   (obtain-document [source]
-    (PDDocument/load source))
+    (load-pdf-from-bytes source))
 
   String
   (obtain-document [source]
-    (load-pdf source))
+    (load-pdf-from-media source))
 
   File
   (obtain-document [source]
-    (load-pdf source))
-
-  PDDocument
-  (obtain-document [source]
-    source))
+    (load-pdf-from-media source)))
 
 (defn get-form
   "Obtain AcroForm from a open `doc`, opened with obtain-document"
