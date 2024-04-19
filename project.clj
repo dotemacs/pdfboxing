cfaf1f6f22a1e0bcc5fc5c5114a1189ec95d7550
Александар Симић <a@repl.ist>
2024-04-19 14:46:04 +0100

    Updated PDFBox & Clojure versions
    Updated to the latest stable

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/dotemacs/pdfboxing"
   :license {:name "BSD"
             :url "https://opensource.org/license/bsd-3-clause/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.apache.pdfbox/pdfbox "3.0.1"]
-                 [org.apache.pdfbox/pdfbox-io "3.0.1"]
-                 [org.apache.pdfbox/pdfbox-tools "3.0.1"]])
+  :dependencies [[org.clojure/clojure "1.11.2"]
+                 [org.apache.pdfbox/pdfbox "3.0.2"]
+                 [org.apache.pdfbox/pdfbox-io "3.0.2"]
+                 [org.apache.pdfbox/pdfbox-tools "3.0.2"]])
